473b03509272143404d04a57744f55b2f958088f
Daniel Limas <amedlimas97@gmail.com>
2020-10-21 16:02:29 -0600

    Wildcard matching too strong

---
+++ 1-100/44-wildcard-matching.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/wildcard-matching/discuss/903975/simple-dp-solution-(any-language)
+
+def wildcard_matching(s, pattern):
+    pass
+
+if __name__ =="__main__":
+    print(wildcard_matching("cb", "?b"))
